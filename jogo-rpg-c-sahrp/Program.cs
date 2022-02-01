fee262d47094e8071c336f7186d3530226500d54
Eder Vieira <eder.edy@gmail.com>
2022-01-31 22:09:40 -0400

    Aula de projeto RPG

--- jogo-rpg-c-sahrp/Program.cs
+++ jogo-rpg-c-sahrp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using jogo_rpg_c_sahrp.src.Entities;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Hero hero = new Hero("Arus", 23, "Knight");
+        Wizard mago = new Wizard("Malefico", 99, "Devil");
+
+        Console.WriteLine(hero.Attack());
+        //Console.WriteLine(mago.Attack());
+    }
+}
