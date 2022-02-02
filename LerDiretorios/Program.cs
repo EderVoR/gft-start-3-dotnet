b1280b070e398a6edc584e924de2163b5e953e7b
Eder Vieira <eder.edy@gmail.com>
2022-02-02 11:48:54 -0400

    Listagem de arquivos e impressão das informações

--- LerDiretorios/Program.cs
+++ LerDiretorios/Program.cs
@@ -23,11 +23,27 @@ public class Program
         else
             Console.WriteLine($"{path} não existe.");
     }
+    public static void LerArquivos(string path)
+    {
+        //Verifica as pastas e subpastas para verificar os arquivos
+        var arquivos = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+        //Verfica todos os arquivos nas pastas e imprime as informações
+        foreach (var arq in arquivos)
+        {
+            var fileInfo = new FileInfo(arq);
+            Console.WriteLine($"[Nome]: {fileInfo.Name}");
+            Console.WriteLine($"[Tamanho]: {fileInfo.Length}");
+            Console.WriteLine($"[Ultimo acesso]: {fileInfo.LastAccessTimeUtc}");
+            Console.WriteLine($"[Diretorio]: {fileInfo.DirectoryName}");
+            Console.WriteLine($"\n----------------------\n");
+        }
+    }
     public static void Main(String[] args)
     {
         //Recebe o diretorio que quer listar as pastas e faz a chama da função de listar
         var path = @"C:\workspace\gft-start-3-dotnet\directory_and_directoryinfo\globo\";
-        LerDiretorios(path);
+        //LerDiretorios(path);
+        LerArquivos(path);
         Console.WriteLine("\nAperte enter para finalizar....");
         Console.ReadLine();
     }
