064eab135b20a092d751589b2c35fb9a5bbd07d6
Eder Vieira <eder.edy@gmail.com>
2022-02-01 22:23:51 -0400

    Estudo de geração de arquivos

--- file_fileinfo/Program.cs
+++ file_fileinfo/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void CriarArquivo(string path)
+    {
+        try
+        {
+            //Cria o arquivo
+            using var sw = File.CreateText(path);
+            //Insere uma linha no arquivo
+            sw.WriteLine("teste de arquivo");
+            //Finaliza o arquivo e salva as alteracoes
+            sw.Flush();            
+        }
+        catch
+        {            
+            Console.WriteLine("O nome do arquivo esta invalido!");
+        }
+    }
+
+    public static string LimparNome(string nome)
+    {
+        //Valida os caracteres invalidos e faz a remoção doe mesmos
+        foreach (var @char in Path.GetInvalidFileNameChars())
+        {
+            //Remove os caracteres invalidos por espaco
+            nome = nome.Replace(@char, ' ');
+        }
+        return nome;
+    }
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Informe o nome do arquivo: ");
+        var nomeArquivo = Console.ReadLine();
+
+        nomeArquivo = LimparNome(nomeArquivo);
+
+        var path = Path.Combine(Environment.CurrentDirectory, $"{nomeArquivo}.txt");
+        
+        CriarArquivo(path);
+
+        Console.WriteLine("Aperte enter para finalizar...");
+        Console.ReadLine();
+    }
+}
+
