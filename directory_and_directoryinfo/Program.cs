ee61790290a05b54da0c4135b1cfeab643c9e11d
Eder Vieira <eder.edy@gmail.com>
2022-02-01 22:51:55 -0400

    Criação de direroios e subdiretorios

---
+++ directory_and_directoryinfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        //Cria o diretorio Globo
+        var path = Path.Combine(Environment.CurrentDirectory, "globo");
+        //Armazena os dados do diretorio Globo
+        var dirGlobo = Directory.CreateDirectory(path);
+        //Cria os subdiretorios baseado nas informacoes do diretorio pai Globo
+        var dirAmNorte = dirGlobo.CreateSubdirectory("América do Norte");
+        var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+        var dirAmSul = dirGlobo.CreateSubdirectory("América do Sul");
+        //Cria os subdiretorios baseados nos pais
+        dirAmNorte.CreateSubdirectory("USA");
+        dirAmNorte.CreateSubdirectory("Canada");
+        dirAmNorte.CreateSubdirectory("Mexico");
+        dirAmCentral.CreateSubdirectory("Panama");
+        dirAmCentral.CreateSubdirectory("Costa Rica");
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Argentina");
+        dirAmSul.CreateSubdirectory("Paraguay");
+    }
+}
