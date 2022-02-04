0021c8d86c5652d7ab7bad9ba2b2ea61aefd2dbd
Eder Vieira <eder.edy@gmail.com>
2022-02-03 22:12:33 -0400

    CsvHelper chamada de bibliotecae leitura de arq

---
+++ Csv_Helper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        LerArquivo();
+        Console.WriteLine("Aperte [enter] para finalizar....");
+        Console.ReadLine();
+        
+        
+        static void LerArquivo()
+        {
+
+                var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "Produtos.csv");
+
+                var fi = new FileInfo(path);
+
+                //Verifica a existencia do arquivo, caso contrario emite um alerta
+                if(!fi.Exists)
+                    throw new FileNotFoundException($"O arquivo {path} não foi localizado.");
+
+                using var sr = new StreamReader(fi.FullName);
+                var csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture);
+                using var csvReader = new CsvHelper.CsvReader(sr, csvConfig);
+
+                var registros = csvReader.GetRecords<dynamic>();
+
+                foreach (var registro in registros)        
+                {
+                    Console.WriteLine($"nome: {registro.Produto}");
+                    Console.WriteLine($"marca: {registro.Marca}");
+                    Console.WriteLine($"preço: {registro.Preco}");
+                    Console.WriteLine("--------------");
+                }
+            }
+    }
+}
