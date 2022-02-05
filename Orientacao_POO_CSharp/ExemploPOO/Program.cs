69a8e458d7098b63c31516954bc6ba7d7c61b05b
Eder Vieira <eder.edy@gmail.com>
2022-02-05 12:33:22 -0400

    Abstração de dados POO em C#

---
+++ Orientacao_POO_CSharp/ExemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExemploPOO.Models;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Pessoa p1 = new Pessoa();
+
+        p1.Nome = "Eder";
+        p1.Idade = 34;
+
+        p1.Apresentar();
+    }
+}
