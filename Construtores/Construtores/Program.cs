ef970ea360dd109e2f8d5f37c6871d7121152f82
Eder Vieira <eder.edy@gmail.com>
2022-02-04 21:47:50 -0400

    Get, Set e suas propriedades

--- Construtores/Construtores/Program.cs
+++ Construtores/Construtores/Program.cs
@@ -5,13 +5,17 @@ public class Program
 {
     public static void Main(String[] args)
     {
+        Data data = new Data();
+        data.SetMes(20);
+
+        data.ApresentarMes();
         // Tratamento de Construtor privado
         // Log log = Log.GetInstance();
         // log.PropriedadeLog = "teste de isntancia";
         // Log log2 = Log.GetInstance();
         // Console.WriteLine($"{log2.PropriedadeLog}");
 
-        Aluno p1 = new Aluno("Eder", "Vieira", "Teste");
-        p1.Apresentar();
+        // Aluno p1 = new Aluno("Eder", "Vieira", "Teste");
+        // p1.Apresentar();
     }
 }
