f3ee46e48f7406977a30768838adb0b2f718e800
Eder Vieira <eder.edy@gmail.com>
2022-02-07 22:52:42 -0400

    Ordenação Bubble Sort

---
+++ ExemplosColecoes/ExemplosColecoes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExemplosColecoes.Helper;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] arrayInteiros = new int[5] {6, 3, 8, 1, 9};
+
+        OperacoesArray op = new OperacoesArray();
+
+        op.OrdenarBubbleSort(ref arrayInteiros);
+        op.ImprimirArray(arrayInteiros);
+    }
+}
